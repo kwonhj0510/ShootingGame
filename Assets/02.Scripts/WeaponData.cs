dc5c7493484ad84ac5b51fa482bac23a54d9edc9
kwonhj0510 <khj96592@gmail.com>
2025-03-19 00:20:59 +0900

    추가 | WeaponData

---
+++ Assets/02.Scripts/WeaponData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponData", menuName = "Game/WeaponData")]
+public class WeaponData : ScriptableObject
+{
+    public string weaponName;       //이름
+    public int damage;              //데미지
+    public int magazine;            //탄창 수
+    public float effectiveRange;    //사정거리
+    public float reloadTime;        //장전시간
+    public float perShot;           //한 발을 발사하는 데 걸리는 시간
+}
