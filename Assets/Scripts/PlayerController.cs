39c6155cbacff05c02c2a0a7f026418a5be6e8fb
kwonhj0510 <khj96592@gmail.com>
2025-03-18 20:37:09 +0900

    추가 | 캐릭터 이동 및 점프 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public CharacterData characterData;
+
+    private float jumpForce = 10f;
+    private float speed;
+    private int hp;
+
+    private Rigidbody2D rb;
+
+    private KeyCode keyCodeJump = KeyCode.Space;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        speed = characterData.speed;
+        hp = characterData.maxHP;
+    }
+
+    private void Update()
+    {
+        Move();
+        if (Input.GetKeyDown(keyCodeJump))
+        {
+            Jump();
+        }
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal"); 
+
+        rb.linearVelocity = new Vector2(x * speed / 10, rb.linearVelocityY);
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = Vector2.up * jumpForce;
     }
 }
