3ca0a05eb689f4edb2e4c5825b3e4c4820c2aa21
kwonhj0510 <khj96592@gmail.com>
2025-03-18 23:55:41 +0900

    추가 |  바닥판정 및 점프 횟수 제한

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,43 @@ public class PlayerController : MonoBehaviour
 {
     public CharacterData characterData;
 
+    private Rigidbody2D rb;
     private float jumpForce = 10f;
     private float speed;
     private int hp;
 
-    private Rigidbody2D rb;
+    [SerializeField] private LayerMask groundLayerMask;
+    private CapsuleCollider2D capsuleCollider;
+    private bool isGrounded;
+    private Vector3 footPosition;
+
+    private int maxJumpCount = 2;
+    private int currentJumpCount = 0;
 
     private KeyCode keyCodeJump = KeyCode.Space;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     private void Start()
     {
         speed = characterData.speed;
         hp = characterData.maxHP;
+    }
+
+    private void FixedUpdate()
+    {
+        Bounds bounds = capsuleCollider.bounds;
+        footPosition = new Vector2(bounds.center.x, bounds.min.y);
+        isGrounded = Physics2D.OverlapCircle(footPosition, 0.1f, groundLayerMask);
+
+        if (isGrounded && rb.linearVelocityY < 0) 
+        {
+            currentJumpCount = maxJumpCount;
+        }
     }
 
     private void Update()
@@ -41,6 +61,10 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        rb.linearVelocity = Vector2.up * jumpForce;
+        if (currentJumpCount > 0)
+        {
+            rb.linearVelocity = Vector2.up * jumpForce;
+            currentJumpCount--;
+        }
     }
 }
